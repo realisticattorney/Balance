0c4768249ea8b7c718a861ea30dc9b6c5b121943
realisticattorney <g.axl.aquila@gmail.com>
2021-03-13 14:38:37 -0300

    Add active record storage blobs to upload images later

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-
+User.create!(email: 'admin@example.com', password: 'admin@example.com', password_confirmation: 'admin@example.com')
 
 30.times do
   Recipe.create!([{
