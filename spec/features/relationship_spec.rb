0d7458c997a56bf651f5c601b0d6cc11fca15a56
realisticattorney <g.axl.aquila@gmail.com>
2021-03-18 03:56:44 -0300

    Comment bullet n+1 query tests on config

--- spec/features/relationship_spec.rb
+++ spec/features/relationship_spec.rb
@@ -2,7 +2,8 @@ require 'rails_helper'
 
 RSpec.feature 'Following', type: :feature do
   before :each do
-    @user1 = User.create!(email: 'german1@gmail.com', password: 'german1')
-    @user2 = User.create!(email: 'axel1@gmail.com', password: 'german1')
+    @user1 = User.create!(fullname: "german aquila", username: "german_aquila", email: 'german@gmail.com', password: 'german1', avatar_photo: '200x200.jpeg', cover_photo: '600x200.jpeg')
+    @userw = User.create!(fullname: "germano aquila", username: "axel_aquila", email: 'axel@gmail.com', password: 'german1', avatar_photo: '200x200.jpeg', cover_photo: '600x200.jpeg')
+    Recipe.create(user_id: @user1.id, description: 'my first post')
   end
 end
